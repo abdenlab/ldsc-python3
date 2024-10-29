a37c1a98a5e2305db0d82ab8d5f2cd50d43af7a8
Thomas Reimonn <thomas.reimonn@umassmed.edu>
2024-10-29 10:18:21 -0400

    run black

--- test/test_munge_sumstats.py
+++ test/test_munge_sumstats.py
@@ -3,8 +3,7 @@ import unittest
 import nose
 import numpy as np
 import pandas as pd
-from numpy.testing import (assert_allclose, assert_array_almost_equal,
-                           assert_array_equal)
+from numpy.testing import assert_allclose, assert_array_almost_equal, assert_array_equal
 from pandas.util.testing import assert_frame_equal, assert_series_equal
 
 import munge_sumstats as munge
